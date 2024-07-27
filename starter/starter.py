1f8855321a12a0b069278f70683dbba492fa3fb8
bandiaoz <893396998@qq.com>
2024-07-27 12:38:32 +0800

    pref: 重构 starter.py

--- starter/starter.py
+++ starter/starter.py
@@ -1,10 +1,15 @@
 import os
 
 
-def compile_cpp(file_name, path="./"):
+def compile_cpp(
+    file_name,
+    path="/Users/chenjinglong/algorithm_competition/nowcoder/template/generator",
+):
     """
     编译 cpp 文件，获得同名可执行文件；例如 std.cpp -> std, checker.cc -> checker
     """
+    if not os.path.exists(file_name):
+        return
     prefix = file_name.split(".")[0]
     os.system(
         f"g++-14 ./{file_name} \
@@ -14,27 +19,24 @@ def compile_cpp(file_name, path="./"):
     )
 
 
+def remove(file_names):
+    for file_name in file_names:
+        os.system(f"rm {file_name} || true")
+
+
 if __name__ == "__main__":
     # 删除之前生成的文件
-    os.system("rm data.zip || true")
-    os.system("rm *.in || true")
-    os.system("rm *.out || true")
-    os.system("rm gen || true")
+    remove(["data.zip", "*.in", "*.out", "gen", "std", "checker"])
 
     # 生成输入输出文件 .in .out
-    compile_cpp("gen.cpp", path="/Users/chenjinglong/algorithm_competition/nowcoder/template/generator")
+    compile_cpp("gen.cpp")
     compile_cpp("std.cpp")
-    if os.path.exists("checker.cc"):
-        compile_cpp("checker.cc", path="/Users/chenjinglong/algorithm_competition/nowcoder/template/generator")
-    
+    compile_cpp("checker.cc")
+
     os.system("./gen")
-    
+
     # 打包成 data.zip
     os.system("zip -q data.zip *.in *.out checker.cc")
     # 删除中间文件
-    os.system("rm gen || true")
-    os.system("rm std || true")
-    os.system("rm checker || true")
-    os.system("rm *.in || true")
-    os.system("rm *.out || true")
-    os.system("rm starter.py || true")
+    remove(["*.in", "*.out", "gen", "std", "checker"])
+    remove([["starter.py"]])

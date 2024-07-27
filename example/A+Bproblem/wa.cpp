27b020d03429471500dd7b17b02bdc9eef4e227f
bandiaoz <893396998@qq.com>
2024-07-27 20:15:42 +0800

    pref: 重构starter.py

---
+++ example/A+Bproblem/wa.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using ll = long long;
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    int a, b;
+    std::cin >> a >> b;
+
+    std::cout << a + b + 1 << '\n';
+
+    return 0;
+}
--- starter/starter.py
+++ starter/starter.py
@@ -1,5 +1,5 @@
 import os
-
+import sys
 
 def compile_cpp(
     file_name,
@@ -18,25 +18,59 @@ def compile_cpp(
         -I{path}"
     )
 
+cpp_files_prefix = []
+def compile_all_cpp(exclude=[]):
+    for file_name in os.listdir("."):
+        if file_name in exclude:
+            continue
+        if file_name.endswith(".cpp"):
+            cpp_files_prefix.append(file_name[:-4])
+            compile_cpp(file_name)
+        elif file_name.endswith(".cc"):
+            cpp_files_prefix.append(file_name[:-3])
+            compile_cpp(file_name)
+    
 
 def remove(file_names):
     for file_name in file_names:
         os.system(f"rm {file_name} || true")
 
 
-if __name__ == "__main__":
+def clear():
+    # 删除所有临时文件
+    remove(["*.in", "*.out"])
+    remove(cpp_files_prefix)
+    cpp_files_prefix.clear()
+
+
+def generator():
     # 删除之前生成的文件
-    remove(["data.zip", "*.in", "*.out", "gen", "std", "checker"])
+    clear()
+    remove(["data.zip"])
 
-    # 生成输入输出文件 .in .out
-    compile_cpp("gen.cpp")
-    compile_cpp("std.cpp")
-    compile_cpp("checker.cc")
-
+    compile_all_cpp(["compare.cpp"])
+    # 生成 *.in 和 *.out 文件
     os.system("./gen")
 
     # 打包成 data.zip
     os.system("zip -q data.zip *.in *.out checker.cc")
-    # 删除中间文件
-    remove(["*.in", "*.out", "gen", "std", "checker"])
-    remove([["starter.py"]])
+    clear()
+
+def compare():
+    # 编译所有 cpp 文件
+    compile_all_cpp(["gen.cpp"])
+    os.system("./compare")
+    clear()
+
+
+func = {
+    "generator": generator,
+    "compare": compare,   
+}
+
+if __name__ == "__main__":
+    args = sys.argv
+    if len(args) > 1:
+        func[args[1]]()
+        
+    remove(["starter.py"])

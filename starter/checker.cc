78bce26d11b7f32acf287031023c07b138f68b54
bandiaoz <893396998@qq.com>
2024-08-10 15:03:04 +0800

    feat: 复制一份 checker.cc 到当前文件夹

---
+++ starter/checker.cc
@@ -0,0 +1,12 @@
+#include "testlib.h"
+#include <bits/stdc++.h>
+
+using ll = long long;
+
+int main(int argc, char* argv[]) {
+    registerTestlibCmd(argc, argv);
+
+    
+ 
+    quitf(_ok, "Accepted!");
+}

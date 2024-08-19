13ce1db9584f055f8458a668365b146347e410e1
bandiaoz <893396998@qq.com>
2024-08-20 00:57:23 +0800

    fix: 调整 val.cpp 模板

--- starter/val.cpp
+++ starter/val.cpp
@@ -5,11 +5,8 @@ using ll = long long;
 int main(int argc, char* argv[]) {
     registerValidation(argc, argv);
 
-    int a = inf.readInt(1, 1e9, "a");
-    inf.readSpace();
-    int b = inf.readInt(1, 1e9, "b");
-    inf.readEoln();
-    inf.readEof();
+    
 
+    inf.readEof();
     return 0;
 }
